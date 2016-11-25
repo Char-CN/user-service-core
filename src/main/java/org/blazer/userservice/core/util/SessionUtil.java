d863922c2a6381eca7b2723fcb5402b59f5053dc
hyy <hyy@192.168.99.1>
2016-11-25 16:22:04 +0800

    1.修复decode时的bug

--- src/main/java/org/blazer/userservice/core/util/SessionUtil.java
+++ src/main/java/org/blazer/userservice/core/util/SessionUtil.java
@@ -16,6 +16,11 @@ public class SessionUtil {
 	 * 分隔符
 	 */
 	public static final String SEPARATOR = ",";
+	
+	public static void main(String[] args) {
+		System.out.println(encode(1480061451914L,3,"heyunyang","何云洋","heyunyang@evergrande.com","",1));
+		System.out.println(decode("2e722b4e56a82eaf5d51218d182d49a8130eaa98213901a60bd9566da91e6513566e15a6a32710c0af87dc83a25b8c5eef105f41726a06b72c55322dd192f4bf"));
+	}
 
 	/**
 	 * 每位的意义：ExpireTime,UserId,UserName,UserNameCn,Email,PhoneNumber,LoginType
@@ -35,6 +40,7 @@ public class SessionUtil {
 	 * @return
 	 */
 	public static String encode(Long expire, Integer id, String userName, String userNameCn, String email, String phoneNumber, Integer loginType) {
+//		System.out.println(String.format(FORMAT, expire, id, userName, userNameCn, email, phoneNumber, loginType));
 		String sessionId = DesUtil.encrypt(String.format(FORMAT, expire, id, userName, userNameCn, email, phoneNumber, loginType));
 		return sessionId;
 	}
@@ -59,7 +65,8 @@ public class SessionUtil {
 			bean.setValid(false);
 			return bean;
 		}
-		String[] content = StringUtils.splitByWholeSeparator(sessionStr, SEPARATOR);
+		String[] content = StringUtils.splitByWholeSeparatorPreserveAllTokens(sessionStr, SEPARATOR);
+//		System.out.println(sessionStr);
 		if (content.length != 7) {
 			bean.setLoginType(null);
 			bean.setUserId(null);
